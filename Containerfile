aa5aa1a5cd6dcdff27e080c9f48dc990ef6f0448
Joseph Marrero <jmarrero@redhat.com>
2023-12-12 12:34:27 -0500

    Update to run code when /health is used

--- Containerfile
+++ Containerfile
@@ -1,9 +1,12 @@
 FROM registry.access.redhat.com/ubi9/python-311
-RUN pip install requests
 WORKDIR /edge_src/
 COPY edge_client.py .
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
 
 ENV CLIENT_ID=
 ENV CLIENT_SECRET=
 
-CMD [ "python", "./edge_client.py"]
+EXPOSE 5000
+CMD ["python", "-m", "flask", "--app", "edge_client.py", "run", "--host=0.0.0.0", "--port=5000"]
--- edge_client.py
+++ edge_client.py
@@ -1,8 +1,12 @@
 import os
 import requests
+from flask import Flask
+
+app = Flask(__name__)
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
+map_server = os.getenv('MAP_SERVER_URL')
 token_auth_url = "https://sso.smartabyarsmartvillage.org/auth/realms/SMARTVILLAGE/protocol/openid-connect/token"
 
 token_auth_payload =  {
@@ -19,37 +23,40 @@ if response.status_code == 200:
 else:
     print(f"Status: {response.status_code}")
 
-
 api_uri = "https://www.smartabyarsmartvillage.org"
 map_data_api = "/api/map-result"
 smart_traffic_light_api= "/api/smart-traffic-light-import"
-
-
+map_data = ""
 headers = {
     'Authorization': f'Bearer {access_token}',
 }
 
+def get_map_data():
+    map_result = requests.get(url=api_uri+map_data_api, headers=headers)
 
-map_result = requests.get(api_uri+map_data_api, headers)
+    if map_result.status_code == 200:
+        map_data = map_result.json()
+        print(f"GET Request Successful. Data: {map_data}")
+    else:
+        print(f"GET Status code: {map_result.status_code}")
 
-if map_result.status_code == 200:
-    map_data = map_result.json()
-    print(f"GET Request Successful. Data: {map_data}")
-else:
-    print(f"GET Status code: {map_result.status_code}")
-
-
-light_data = {
+def update_data():
+    light_data = {
     'operationId': 'putimportSmartTrafficLight',
-    'x-vertx-event-bus': 'smartabyar-smartvillage-enUS-SmartTrafficLight',
-    #'KEY from above data': 'value from get data',
-}
+        'x-vertx-event-bus': 'smartabyar-smartvillage-enUS-SmartTrafficLight',
+        #'KEY from above data': 'value from get data',
+    }
+    put_response = requests.put(url=map_server+smart_traffic_light_api, data=light_data, headers=headers)
 
-put_response = requests.put(api_uri+smart_traffic_light_api, light_data, headers)
+    if put_response.status_code == 201:
+        # Process the response data for a successful POST request
+        put_data = put_response.json()
+        print(f"Successful PUT, Updated: {put_data}")
+    else:
+        print(f"PUT Failed: {put_response.status_code}, {put_response.text}")
 
-if put_response.status_code == 201:
-    # Process the response data for a successful POST request
-    put_data = put_response.json()
-    print(f"Successful PUT, Updated: {put_data}")
-else:
-    print(f"PUT Failed: {put_response.status_code}, {put_response.text}")
+@app.route('/health')
+def check_data():
+    get_map_data()
+    update_data()
+    return "App is running"
