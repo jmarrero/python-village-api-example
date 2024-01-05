b40df3ecdc4f9304e2cee2b51652a703e77b6d7c
Joseph Marrero <jmarrero@redhat.com>
2024-01-05 08:25:02 -0500

    Use coodinates for put payload

--- edge_client.py
+++ edge_client.py
@@ -9,6 +9,10 @@ client_secret = os.getenv('CLIENT_SECRET')
 map_server = os.getenv('MAP_SERVER_URL')
 token_auth_url = "https://sso.smartabyarsmartvillage.org/auth/realms/SMARTVILLAGE/protocol/openid-connect/token"
 
+if client_id is None or client_secret is None:
+    print("No CLIENT_ID or CLIENT_SECRET environment variable or defined...Exiting")
+    exit(1)
+
 token_auth_payload =  {
     'grant_type': 'client_credentials',
     'client_id': client_id,
@@ -24,39 +28,65 @@ else:
     print(f"Status: {response.status_code}")
 
 api_uri = "https://www.smartabyarsmartvillage.org"
-map_data_api = "/api/map-result"
+map_data_api = "/api/map-result?rows=100&fq=dateTime:2024-02-29T13%3A59%3A58.000[America%2FNew_York]"
 smart_traffic_light_api= "/api/smart-traffic-light-import"
-map_data = ""
-headers = {
-    'Authorization': f'Bearer {access_token}',
-}
 
 def get_map_data():
+    headers = {
+    'Authorization': f'Bearer {access_token}',
+    }
     map_result = requests.get(url=api_uri+map_data_api, headers=headers)
 
     if map_result.status_code == 200:
         map_data = map_result.json()
-        print(f"GET Request Successful. Data: {map_data}")
+        print("GET Request Successful.")
+    #    print(f"GET Request Successful. Data: {map_data}")
     else:
         print(f"GET Status code: {map_result.status_code}")
+    return map_data
 
-def update_data():
-    light_data = {
-    'operationId': 'putimportSmartTrafficLight',
-        'x-vertx-event-bus': 'smartabyar-smartvillage-enUS-SmartTrafficLight',
-        #'KEY from above data': 'value from get data',
+def update_data(map_data):
+    headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Content-Type': 'application/json',
     }
-    put_response = requests.put(url=map_server+smart_traffic_light_api, data=light_data, headers=headers)
 
-    if put_response.status_code == 201:
-        # Process the response data for a successful POST request
-        put_data = put_response.json()
-        print(f"Successful PUT, Updated: {put_data}")
+    all_coordinates = []
+    for item in map_data['list']:
+        if 'location' in item and 'coordinates' in item['location']:
+            coordinates = item['location']['coordinates']
+            all_coordinates.append(coordinates)
+
+    light_data = {
+        "pk": "veberod-intersection-1",
+        "saves": ["entityId", "smartTrafficLightName", "location", "areaServed"],
+        "entityId": "urn:ngsi-ld:SmartTrafficLight:SmartTrafficLight-veberod-intersection-1",
+        "smartTrafficLightName": "Veberöd intersection 1",
+        "location": {
+            "type": "Point",
+            "coordinates": []
+        },
+        "areaServed": [
+        ]
+    }
+
+    light_data['location']['coordinates'] = all_coordinates[0] if all_coordinates else []
+    light_data['areaServed'] = [{"type": "Point", "coordinates": coordinates} for coordinates in all_coordinates[1:]]
+
+    if map_server is not None:
+        put_response = requests.put(url=map_server+smart_traffic_light_api, data=light_data, headers=headers)
+        if put_response.status_code == 201:
+            put_data = put_response.json()
+            print(f"Successful PUT, Updated: {put_data}")
+        else:
+            print(f"PUT Failed: {put_response.status_code}, {put_response.text}")
     else:
-        print(f"PUT Failed: {put_response.status_code}, {put_response.text}")
+        print("No MAP_SERVER_URL environment variable defined")
+        print("Printing payload to terminal:")
+        print(light_data)
 
 @app.route('/health')
 def check_data():
-    get_map_data()
-    #update_data()
+    map_data = get_map_data()
+    update_data(map_data)
     return "App is running"
